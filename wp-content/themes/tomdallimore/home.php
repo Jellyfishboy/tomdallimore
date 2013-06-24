0bbe029c674450edb0b0cffb32a62db0ddbb688b
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-06-24 20:59:10 +0100

    Finished nav menu, footer and started homepage build

--- wp-content/themes/tomdallimore/home.php
+++ wp-content/themes/tomdallimore/home.php
@@ -9,7 +9,35 @@ Template Name: HomeTemp
 </div>
 <div id="content_divider"></div>
 <section class="content">
-    <div class="container">
+    <div class="container" id="home">
+    	<div class="row">
+    		<div class="fourcol">
+    			<div></div>
+    			<h1>What I love</h1>
+    			<p>Armed with the latest technologies and best practises in the industry, I can turn your designs into web reality. I specialise in Ruby on Rails for web applications and HAML, SASS with Compass and Coffeescript for front-end development.</p>
+    		</div>
+    		<div class="fourcol">
+    			<div></div>
+    			<h1>What I love</h1>
+    			<p>I am obsessed with UI design and innovative technologies. I strive to make everyday user experiences as intuitive and efficient as possible, which prove to be both functional and visually stunning.</p>
+    		</div>
+    		<div class="fourcol last">
+    			<div></div>
+    			<h1>Where I reside</h1>
+    			<p>I work as a UX developer for Dyson in the somerset area, however I normally take residence in the lively city of Bristol. When I’m not hammering at the keyboard, I like to explore various music genres and catch some rays.</p>
+    		</div>
+    	</div>
+        <div class="row">
+            <div class="fourcol">
+                <span></span>
+            </div>
+            <div class="fourcol">
+                <h1>Recent work</h1>
+            </div>
+            <div class="fourcol last">
+                <span></span>
+            </div>
+        </div>
 
 
 <?php get_footer('no-sidebar'); ?>
