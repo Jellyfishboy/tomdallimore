b40bcf4129d3c0d39c91eb07e0ea791b353dfcac
Jellyfishboy <Dallas671#>
2013-07-03 13:18:26 +0100

    Added search results and category pages, along with relatable data headers for tag, category and search

---
+++ wp-content/themes/tomdallimore/category.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<?php
+$cate = single_cat_title('', false); ?>
+<div id="content_divider"></div>
+<section class="content">
+    <div class="container blog_category" id="blog">
+        <div class="row">
+            <div class="threecol last">
+                <?php include (TEMPLATEPATH . '/searchform.php'); ?>
+                <h3>Topics</h3>
+                <?php $args = array( 'title_li' => __( '' )); ?>
+                <ul>
+                    <?php wp_list_categories( $args ); ?> 
+                </ul>
+            </div>
+            <div class="ninecol">
+                <h1 id="post_type">Category: <?php echo $cate ?></h1>
+            </div>
+            <?php $wp_query = new WP_Query();
+            $wp_query->query('&showposts=5&category_name='.$cate.''.'&paged='.$paged);
+            while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+            <article class="ninecol">
+                <div class="row">
+                    <div class="twocol">
+                        <a href="<?php the_permalink() ?>"><h2 class="<?php $category = get_the_category(); echo $category[0]->slug; ?>"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></h2></a>
+                    </div>
+                    <div class="tencol last">
+                        <a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+                        <p><?php the_excerpt(); ?></p>
+                        <p><?php the_time('dS F Y') ?></p>
+                        <div class="tags">
+                            <?php $before = ''; the_tags($before, $separator, $after); ?>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <?php endwhile;?>
+        </div>
+<?php get_footer('no-sidebar'); ?>
--- wp-content/themes/tomdallimore/tag.php
+++ wp-content/themes/tomdallimore/tag.php
@@ -1,24 +1,39 @@
 <?php get_header(); ?>
 <?php
 $tags = single_tag_title('', false); ?>
-<?php $wp_query = new WP_Query();
-$wp_query->query('tag='.$tags.''.'&showposts=5'.'&paged='.$paged);
- while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-<div class="row blog_posts post_border">
-	<div class="twocol post_info">
-		<a href="<?php the_permalink() ?>"><h4 class="<?php $category = get_the_category(); echo $category[0]->slug; ?>"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></h4></a>
-		<i class="icon-calendar"></i>
-		<p><?php the_time('dS F Y') ?></p>
-		<i class="icon-comments-alt"></i>
-		<p><?php comments_number(); ?></p>
-	</div>
-	<div class="eightcol post_content">
-		<div class="header2"><img class="star" src="<?php echo bloginfo('template_directory'); ?>/assets/images/star.png"/><a href="<?php the_permalink() ?>"><h2><?php the_title(); ?></h2></a></div>
-		<p><?php the_excerpt(); ?></p>
-	</div>
-	<div class="twocol last tags">
-		<?php $before = ''; the_tags($before, $separator, $after); ?>
-	</div>
-</div>
-<?php endwhile; ?>
+<div id="content_divider"></div>
+<section class="content">
+    <div class="container blog_tags" id="blog">
+    	<div class="row">
+    		<div class="threecol last">
+    			<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+    			<h3>Topics</h3>
+    			<?php $args = array( 'title_li' => __( '' )); ?>
+    			<ul>
+	    			<?php wp_list_categories( $args ); ?> 
+	    		</ul>
+    		</div>
+            <div class="ninecol">
+                <h1 id="post_type">Tag: <?php echo $tags ?></h1>
+            </div>
+			<?php $wp_query = new WP_Query();
+			$wp_query->query('&tag='.$tags.'&category_name=development,article,link');
+			while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+			<article class="ninecol">
+				<div class="row">
+					<div class="twocol">
+						<a href="<?php the_permalink() ?>"><h2 class="<?php $category = get_the_category(); echo $category[0]->slug; ?>"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></h2></a>
+					</div>
+					<div class="tencol last">
+						<a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+						<p><?php the_excerpt(); ?></p>
+						<p><?php the_time('dS F Y') ?></p>
+						<div class="tags">
+							<?php $before = ''; the_tags($before, $separator, $after); ?>
+						</div>
+					</div>
+				</div>
+			</article>
+			<?php endwhile;?>
+		</div>
 <?php get_footer('no-sidebar'); ?>
