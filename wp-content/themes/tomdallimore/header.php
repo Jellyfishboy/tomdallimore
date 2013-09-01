32c9c433df26c14dd64861b25bf257045cd268f6
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-09-02 00:12:04 +0100

    Fixed header links

--- wp-content/themes/tomdallimore/header.php
+++ wp-content/themes/tomdallimore/header.php
@@ -69,10 +69,10 @@
      ?>">
     			            <ul id="menu-main_nav" class="">
                                 <li id="menu-item-80" class="menu-item menu-item-type-post_type menu-item-object-page <?php if (is_page(8)) { echo 'current_page_item'; } ?> page-item-8 menu-item-80">
-                                    <a href="http://localhost:8888/" class="icomoon-notebook"></a>
+                                    <a href="/" class="icomoon-notebook"></a>
                                 </li>
                                 <li id="menu-item-81" class="menu-item menu-item-type-post_type menu-item-object-page <?php if (is_page(6)) { echo 'current_page_item'; } ?> menu-item-81 page-item-6">
-                                    <a href="http://localhost:8888/about/" class="icomoon-user"></a>
+                                    <a href="/about/" class="icomoon-user"></a>
                                 </li>
                             </ul>
     			        </nav>
