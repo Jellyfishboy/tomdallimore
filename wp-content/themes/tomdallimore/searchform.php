e93436a3501bc78dbc367bd7ab0255b0ed7b9138
Jellyfishboy <Dallas671#>
2013-09-02 16:35:16 +0100

    Fixed search form bug

--- wp-content/themes/tomdallimore/searchform.php
+++ wp-content/themes/tomdallimore/searchform.php
@@ -6,7 +6,7 @@
  * @subpackage Tom Dallimore
  */
 ?>
-<form role="search" method="get" id="searchform" action="http://localhost:8888/">
+<form role="search" method="get" id="searchform" action="http://tomdallimore.com/">
     <div>
         <input type="text" value="" name="s" id="s" placeholder="Find more posts" /><i class="icon-search"></i>
         <input type="submit"/>
