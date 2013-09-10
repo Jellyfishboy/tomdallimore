a0d1ea3364ff5c5913ce09d09b73cf4426283de8
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-09-11 00:26:09 +0100

    Changed about copy

--- wp-content/themes/tomdallimore/about.php
+++ wp-content/themes/tomdallimore/about.php
@@ -70,7 +70,7 @@ if(isset($_POST['submitted'])) {
         <div class="sixcol">
             <span></span>
             <h1>Hi there, my name is Tom</h1>
-            <p>I was born and raised in the infamous city known as Bristol. Since growing up within the industry of my father’s Graphic Design company, I have developed an unhealthy obsession to all things related to technology and the web.</p>
+            <p>I was born and raised in the renowned city of Bristol. Since growing up within the industry of my father’s Graphic Design company, I have developed an unhealthy obsession to all things related to technology and the web.</p>
             <p>I decided to channel this passion by learning UI design and web development. Refining my skills over the course of six years, I have flourished into an ambitious business-driven individual.</p>
             <p>I have spent the past few years freelancing and contracting for some exciting companies including Dyson, Blak Pearl and Future PLC.</p>  
             <a href="http://www.tomdallimore.com/wp-content/uploads/2013/09/cv_aug_20132.pdf" target="_blank" class="btn btn-primary">View my CV</a>
