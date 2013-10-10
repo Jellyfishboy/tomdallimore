f228db9043d0480af68ebec11d69532c303e9a38
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-10-10 23:00:40 +0100

    Added 404.php page to git repo

---
+++ wp-content/themes/tomdallimore/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<section class="content">
+    <div class="container">
+        <div class="row">
+            <div class="twocol"></div>
+            <div class="eightcol">
+                <img src="<?php echo bloginfo('template_directory'); ?>/assets/img/angry-cat.png" />
+            </div>
+            <div class="twocol last"></div>
+<?php get_footer('no-sidebar'); ?>
