456c0966d5551aaba24b56a533204afdab66f5d4
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-12-12 23:39:15 +0000

    Changed link replacement in twitter.php

--- wp-content/themes/tomdallimore/twitter.php
+++ wp-content/themes/tomdallimore/twitter.php
@@ -5,32 +5,26 @@
       if($tweet['text']){
         echo '<li>';
           $the_tweet = $tweet['text'];
-
-          // if(is_array($tweet['entities']['user_mentions'])){
-          //     foreach($tweet['entities']['user_mentions'] as $key => $user_mention){
-          //         $the_tweet = preg_replace(
-          //             '/@'.$user_mention['screen_name'].'/i',
-          //             '<a href="http://www.twitter.com/'.$user_mention['screen_name'].'" target="_blank">@'.$user_mention['screen_name'].'</a>',
-          //             $the_tweet);
-          //     }
-          // }
-          // if(is_array($tweet['entities']['hashtags'])){
-          //     foreach($tweet['entities']['hashtags'] as $key => $hashtag){
-          //         $the_tweet = preg_replace(
-          //             '/#'.$hashtag['text'].'/i',
-          //             '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'&src=hash" target="_blank">#'.$hashtag['text'].'</a>',
-          //             $the_tweet);
-          //     }
-          // }
-          // if(is_array($tweet['entities']['urls'])){
-          //     foreach($tweet['entities']['urls'] as $key => $link){
-          //         $the_tweet = preg_replace(
-          //             '`'.$link['url'].'`',
-          //             '<a href="'.$link['url'].'" target="_blank">'.$link['url'].'</a>',
-          //             $the_tweet);
-          //     }
-          // }
-
+          foreach($tweet['entities']['user_mentions'] as $key => $user_mention){
+              $the_tweet = preg_replace(
+                  '/@'.$user_mention['screen_name'].'/i',
+                  '<a href="http://www.twitter.com/'.$user_mention['screen_name'].'" target="_blank">@'.$user_mention['screen_name'].'</a>',
+                  $the_tweet);
+          }
+          foreach($tweet['entities']['hashtags'] as $key => $hashtag){
+              $the_tweet = preg_replace(
+                  '/#'.$hashtag['text'].'/i',
+                  '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'&src=hash" target="_blank">#'.$hashtag['text'].'</a>',
+                  $the_tweet);
+          }
+          foreach($tweet['entities']['urls'] as $key => $link){
+              $the_tweet = preg_replace(
+                  '`'.$link['url'].'`',
+                  '<a href="'.$link['url'].'" target="_blank">'.$link['url'].'</a>',
+                  $the_tweet);
+          }
+          // $the_tweet = preg_replace('/http:\/\/([a-z0-9_\.\-\+\&\!\#\~\/\,]+)/i', '<a href="http://$1" target="_blank">http://$1</a>', $the_tweet); //replace links
+          // $the_tweet = preg_replace('/@([a-z0-9_]+)/i', '<a href="http://twitter.com/$1" target="_blank">@$1</a>', $the_tweet ); //replace users
           echo $the_tweet;
           echo '<span>'.humanTiming(strtotime($tweet['created_at'])) . ' ago</span>';
           echo '</li>';
