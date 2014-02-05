d0c552de31eb95923f89ba1c02ef70234f06199a
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-02-05 00:17:35 +0000

    Changed flickr API url to stop the key from expiring.

--- wp-content/themes/tomdallimore/assets/js/application.js
+++ wp-content/themes/tomdallimore/assets/js/application.js
@@ -8,7 +8,7 @@
     });
     $("#twitter_div a").attr("target", "_blank");
     $('#blog .tencol p a').attr('target', '_blank');
-    url = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=31d03fb6defae4a7e134b97320a30360&photoset_id=72157640433123824&per_page=15&format=json&nojsoncallback=1&auth_token=72157640431165964-4481186fc0a3dc57&api_sig=681d0ca75ca84278945be7121a9c4057";
+    url = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=8d0b4b96dcede3850ffb7409076c507e&photoset_id=72157640433123824+&per_page=15&format=json&nojsoncallback=1";
     $.getJSON(url, function(res) {
       var photo, _i, _len, _ref, _results;
       _ref = res.photoset.photo;
