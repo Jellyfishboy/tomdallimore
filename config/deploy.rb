90693c5be9c5645670254fd465b739fd2b110d37
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-05-06 18:42:37 +0100

    Modified deployment configuration to copy the wp-config-production file into the relative application directory.

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :configure do
     end
     desc "Copy production config file across"
     task :database, :roles => :app do 
-        run "cp /var/www/wpconfig/tomdallimore/wp-config-production.php /var/www/tomdallimore/current"
+        run "cp /var/www/wpconfig/tomdallimore/wp-config-production.php {application_dir}/current"
     end
 end
 namespace :permissions do
