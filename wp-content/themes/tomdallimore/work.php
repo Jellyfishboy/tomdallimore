f5c1ae62717894bbbdc6df6f5930b56692b80d24
Tom Dallimore <me@tomdallimore.com>
2016-03-09 02:56:13 +0700

    Fixed skill icons and removed hero content on work template.

--- wp-content/themes/tomdallimore/work.php
+++ wp-content/themes/tomdallimore/work.php
@@ -4,49 +4,6 @@ Template Name: WorkTemp
 */ 
 ?>
 <?php get_header(); ?>
-<div id="work_focus" class="focus_content">
-	<?php $wp_query = new WP_Query();
-			$wp_query->query('&category_name=featured');
-			while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-		<div class="row">
-			<div class="sixcol screenshot">
-				<h1><?php the_title(); ?></h1>
-				<ul class="skills">
-					<?php
-						$posttags = get_the_tags();
-						if ($posttags) {
-						  foreach($posttags as $tag) {
-						    echo '<li class="' . $tag->name . '" data-toggle="tooltip" data-placement="bottom" data-original-title="' . $tag->name . '"></li>'; 
-						  }
-						}
-					?>
-				</ul>
-				<a href="http://soca.tomdallimore.com" target="_blank" class="btn btn-success btn-large"><i class="icon-earth"></i>Website</a>
-				<a href="https://github.com/Jellyfishboy/soca" target="_blank" class="btn btn-black btn-large"><i class="icon-github"></i>Repository</a>
-				<?php if (has_post_thumbnail( $post->ID ) ): ?>
-					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-					<img src="<?php echo $image[0]; ?>" />
-				<?php endif; ?>
-			</div>
-			<div class="sixcol last">
-				<h1><?php the_title(); ?></h1>
-				<p><?php the_content(); ?></p>
-				<ul class="skills">
-					<?php
-						$posttags = get_the_tags();
-						if ($posttags) {
-						  foreach($posttags as $tag) {
-						    echo '<li class="' . $tag->name . '" data-toggle="tooltip" data-placement="bottom" data-original-title="' . $tag->name . '"></li>'; 
-						  }
-						}
-					?>
-				</ul>
-				<a href="http://soca.tomdallimore.com" target="_blank" class="btn btn-success btn-large"><i class="icon-earth"></i>Website</a>
-				<a href="https://github.com/Jellyfishboy/soca" target="_blank" class="btn btn-black btn-large"><i class="icon-github"></i>Repository</a>
-			</div>
-		</div>
-	<?php endwhile;?>
-</div>
 <section class="content">
     <div class="container blog_loop" id="work" data-work="true">
 			<?php $wp_query = new WP_Query();
